151cf64291b119c26c4531a5d3e810e751850d30
Julian Popescu <jpopesculian@gmail.com>
2020-06-25 11:54:33 +0200

    updated README and bumped version

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 A simple Client for the Lucid KV
 
+Currently supports operations for `get`, `put`, `delete` and `exists`.
+`lock`, `unlock`, `increment`, `decrement` and `ttl` are still being implemented.
+
+At the moment there is no support for Authorization.
+
 License: MIT OR Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 //! A simple Client for the Lucid KV
+//!
+//! Currently supports operations for `get`, `put`, `delete` and `exists`.
+//! `lock`, `unlock`, `increment`, `decrement` and `ttl` are still being implemented.
+//!
+//! At the moment there is no support for Authorization.
 
 #[macro_use]
 extern crate failure;
